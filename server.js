0c7d4d71269d4afffb3ee2cc8cdd0abfb768cbec
melvinren <melvin_ren@icloud.com>
2017-07-24 22:00:43 +0800

    add mutataion code

--- server.js
+++ server.js
@@ -4,11 +4,40 @@ var { buildSchema } = require('graphql');
 
 // Construct a schema, using GraphQL shcema language
 var schema = buildSchema(`
+  input MessageInput {
+    content: String
+    author: String
+  }
+
+  type Message {
+    id: ID!
+    content: String
+    author: String
+  }
+
    type Query {
        hello:String
        rollDice(numDice: Int!, numSides: Int): [Int]
+       getMessage(id: ID!): Message
+   }
+
+   type Mutation {
+    createMessage(input: MessageInput): Message
+    updateMessage(id:ID!, input: MessageInput): Message
    }
 `); 
+
+ // If Message had any complex fields, we'd put them on this object.
+ class Message{
+  constructor(id, {content, author}) {
+    this.id = id;
+    this.content = content;
+    this.author = author;
+  }
+ }
+
+// Maps username to content
+var fakeDatabase = {};
 
 // The root provides a resolver function for each API endpoint
 var root = {
@@ -17,6 +46,25 @@ var root = {
    },
    rollDice: (args) => {
        return [args.numDice, args.numSides];
+   },
+   getMessage:function({id}){
+    if(!fakeDatabase[id]){
+      throw new Error('no message exists with id'+id)
+    }
+    return new Message(id, fakeDatabase[id]);
+   },
+   createMessage:function({input}){
+    // random id
+    var id = require('crypto').randomBytes(10).toString('hex');
+    fakeDatabase[id] = input;
+    return new Message(id, input);
+   },
+   updateMessage:function({id, input}){
+    if(!fakeDatabase[id]){
+      throw new Error('no message exists with id'+id)
+    }
+    fakeDatabase[id] = input;
+    return new Message(id, input);
    }
 };
 
